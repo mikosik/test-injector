29c2874e814afde60b73fc3e8c0e4594c8c1ffac
mikosik <marcin.mikosik@gmail.com>
2013-12-10 16:26:17 +0100

    refactored InjectedTestCase to extend AbstractModule instead of implementing Module

--- src/java/com/perunlabs/testinjector/InjectedTestCase.java
+++ src/java/com/perunlabs/testinjector/InjectedTestCase.java
@@ -7,13 +7,12 @@ package com.perunlabs.testinjector;
 
 import org.junit.Before;
 
-import com.google.inject.Binder;
-import com.google.inject.Module;
+import com.google.inject.AbstractModule;
 import com.perunlabs.testinjector.inject.TestInjector;
 
-public class InjectedTestCase implements Module {
+public class InjectedTestCase extends AbstractModule {
   @Override
-  public void configure(Binder binder) {}
+  protected void configure() {}
 
   @Before
   public void performTestInjection() throws Exception {
